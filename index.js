295a499e916b43c69abe9fefcf5080321f92966a
Aman Khandelwal <70818283+Aman2087@users.noreply.github.com>
2020-10-10 13:36:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1 = Math.floor(Math.random()*6 +1);
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = randomDiceImage;
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource );
 
 var randomNumber2 = Math.floor(Math.random()*6 +1);
 var randomDiceImage = "dice" + randomNumber2 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = randomDiceImage;
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource );
 
 if(randomNumber1===randomNumber2)
